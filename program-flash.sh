a0ec660296c5575eec87fa16870b74a2c8a01246
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2019-12-26 17:01:22 +0300

    [A] сборка: скрипт сборки CMakeLists.txt модифицирован чтобы запускать скрипт program-flash.sh для программирования флешки. Крипт должен иметь параметры и не зависеть от конкретных путей, как это есть сейчас.

--- program-flash.sh
+++ program-flash.sh
@@ -1,1 +1,1 @@
-openocd -f openocd.cfg -c "init; reset halt;  flash write_image erase ../build/debug-dev.elf; reset; exit"
+openocd -f ../openocd.cfg -c "init; reset halt;  flash write_image erase debug-dev.elf; reset; exit"
