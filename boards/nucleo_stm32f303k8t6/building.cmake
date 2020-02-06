c1fd8399c1a5b5dd21a5d4289d51f07be499319f
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-02-06 11:50:16 +0300

    [C] сборка и архитектура: изменена архитектура файлов исходников, скриптов сборки и скриптов OOCD, изменено содержимое соответствующих файлов. Не тестировалось.

--- boards/nucleo_stm32f303k8t6/building.cmake
+++ boards/nucleo_stm32f303k8t6/building.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.5)
 
-set(CURRENT_BOARD_TYPE "nucleo_board_f303k8t6")
+set(CURRENT_BOARD_TYPE "nucleo_stm32f303k8t6")
 
 # Если тип платформы не установлен, то завершаем выполнение скрипта
 if (NOT DEFINED BOARD_TYPE)
@@ -16,20 +16,40 @@ endif ()
 
 set(BOARD_TYPE_STATUS "SET")
 
-message(STATUS "BOARD_TYPE: ${BOARD_TYPE}")
+#######################################################################
+## Подключение файло исходных кодов и заголовков
+set(MAIN_ASM_SOURCES    "boards/nucleo_stm32f303k8t6/source/startup_stm32f303x8.s" )
 
-set(MCU_MAJOR_MODEL "STM32F3xx")
+set(MAIN_SOURCES	"${MAIN_SOURCES}"
+					"boards/nucleo_stm32f303k8t6/source/stm32f3xx_it.c"
+					"boards/nucleo_stm32f303k8t6/source/system_stm32f3xx.c"
+					
+set(DEVICE_INCLUDE "base/Device/ST/STM32F3xx/Include")
 
-set(MCU_MINOR_MODEL "STM32F303x8")
+set(DRIVER_INCLUDE "base/Driver/ST/stm32f3xx/include")
 
-set(USE_FULL_LL_DRIVER "y")
+aux_source_directory(DRIVER_SOURCES "base/Driver/ST/stm32f3xx/sources")
 
+#######################################################################
+## Настройка параметров сбоки и компоновки
+
+set(CMAKE_C_FLAGS	${CMAKE_C_FLAGS} "-mcpu=cortex-m4")
+	
+set(CMAKE_ASM_FLAGS	${CMAKE_ASM_FLAGS} "-mcpu=cortex-m4")
+	
+set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS}	"-T boards/nucleo_stm32f303k8t6/flash.ld \
+														-mcpu=cortex-m4 -specs=nano.specs \
+														-Wl,--gc-sections")
+								
+add_definitions("-DSTM32F303x8")
+
+#######################################################################
 # Определение дополнительной цели для выполнения операции прошивки
 add_custom_target("flash" DEPENDS ${PROJ_NAME})
 
 # Переменная описывает имя и положение фала с конфигурацией OOCD для работы с конкретной платформой-процессором
 # Смотреть FLASHER_TYPE в README.md
-set(OOCD_CONFIG "${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_board_f303k8t6_stlinkv2-1.cfg")
+set(OOCD_CONFIG "boards/nucleo_stm32f303k8t6/oocd_stlinkv2-1.cfg")
 
 # Определение команд для цели flash
 add_custom_command(	TARGET "flash"
