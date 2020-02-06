1e41bfc4bf6353a37b12e4c4f11dd510e66d79d3
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-02-06 14:42:39 +0300

    [F] сборка: исправлены ошибки сборки при новой архитектуре.

--- boards/nucleo_stm32f303k8t6/building.cmake
+++ boards/nucleo_stm32f303k8t6/building.cmake
@@ -18,30 +18,31 @@ set(BOARD_TYPE_STATUS "SET")
 
 #######################################################################
 ## Подключение файло исходных кодов и заголовков
-set(MAIN_ASM_SOURCES    "boards/nucleo_stm32f303k8t6/source/startup_stm32f303x8.s" )
+set(MAIN_ASM_SOURCES    "${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_stm32f303k8t6/source/startup_stm32f303x8.s" )
 
 set(MAIN_SOURCES	"${MAIN_SOURCES}"
-					"boards/nucleo_stm32f303k8t6/source/stm32f3xx_it.c"
-					"boards/nucleo_stm32f303k8t6/source/system_stm32f3xx.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_stm32f303k8t6/source/stm32f3xx_it.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_stm32f303k8t6/source/system_stm32f3xx.c")
 					
-set(DEVICE_INCLUDE "base/Device/ST/STM32F3xx/Include")
+set(DEVICE_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/base/Device/ST/STM32F3xx/Include")
 
-set(DRIVER_INCLUDE "base/Driver/ST/stm32f3xx/include")
+set(DRIVER_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/base/Driver/ST/stm32f3xx/include")
 
-aux_source_directory(DRIVER_SOURCES "base/Driver/ST/stm32f3xx/sources")
+file(GLOB DRIVER_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/base/Driver/ST/stm32f3xx/source/*_ll_*.c")
 
 #######################################################################
 ## Настройка параметров сбоки и компоновки
 
-set(CMAKE_C_FLAGS	${CMAKE_C_FLAGS} "-mcpu=cortex-m4")
+set(CMAKE_C_FLAGS	"${CMAKE_C_FLAGS} -mcpu=cortex-m4")
 	
-set(CMAKE_ASM_FLAGS	${CMAKE_ASM_FLAGS} "-mcpu=cortex-m4")
+set(CMAKE_ASM_FLAGS	"${CMAKE_ASM_FLAGS} -mcpu=cortex-m4")
 	
-set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS}	"-T boards/nucleo_stm32f303k8t6/flash.ld \
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}	-T ${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_stm32f303k8t6/flash.ld \
 														-mcpu=cortex-m4 -specs=nano.specs \
 														-Wl,--gc-sections")
 								
 add_definitions("-DSTM32F303x8")
+add_definitions("-DUSE_FULL_LL_DRIVER")
 
 #######################################################################
 # Определение дополнительной цели для выполнения операции прошивки
@@ -49,7 +50,7 @@ add_custom_target("flash" DEPENDS ${PROJ_NAME})
 
 # Переменная описывает имя и положение фала с конфигурацией OOCD для работы с конкретной платформой-процессором
 # Смотреть FLASHER_TYPE в README.md
-set(OOCD_CONFIG "boards/nucleo_stm32f303k8t6/oocd_stlinkv2-1.cfg")
+set(OOCD_CONFIG "${CMAKE_CURRENT_SOURCE_DIR}/boards/nucleo_stm32f303k8t6/oocd_stlinkv2-1.cfg")
 
 # Определение команд для цели flash
 add_custom_command(	TARGET "flash"
