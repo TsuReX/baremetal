48070403e603c6e8c432d789d49479f82d062034
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2020-02-09 16:54:59 +0300

    [A] i2c: изменена функция i2c1_config(), добавлена функция i2c_receive().

--- source/i2c.c
+++ source/i2c.c
@@ -7,51 +7,151 @@
  */
 
 #include "i2c.h"
+#include "console.h"
 
-/** Timing register value is computed with the STM32CubeMX Tool */
-#define I2C_TIMING	0x2000090E
+#define GUARD_COUNTER_INIT 10000
 
 void i2c1_config(void)
 {
-	LL_I2C_InitTypeDef I2C_InitStruct;
-
-	LL_GPIO_InitTypeDef GPIO_InitStruct;
-
-	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-	/* Peripheral clock enable */
-	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
-
 	/** I2C1 GPIO Configuration
 	 PB6   ------> I2C1_SCL
 	 PB7   ------> I2C1_SDA
 	 */
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-	GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
-	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-	GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
-	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
-	/* USART1 interrupt Init */
+	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_6, LL_GPIO_MODE_ALTERNATE);
+	LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_6, LL_GPIO_AF_4);
+	LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_6, LL_GPIO_SPEED_FREQ_HIGH);
+	LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_6, LL_GPIO_OUTPUT_PUSHPULL);
+	LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_6, LL_GPIO_PULL_UP);
+
+	LL_GPIO_SetPinMode(GPIOA, LL_GPIO_PIN_7, LL_GPIO_MODE_ALTERNATE);
+	LL_GPIO_SetAFPin_0_7(GPIOA, LL_GPIO_PIN_7, LL_GPIO_AF_4);
+	LL_GPIO_SetPinSpeed(GPIOA, LL_GPIO_PIN_7, LL_GPIO_SPEED_FREQ_HIGH);
+	LL_GPIO_SetPinOutputType(GPIOA, LL_GPIO_PIN_7, LL_GPIO_OUTPUT_PUSHPULL);
+	LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_7, LL_GPIO_PULL_UP);
+
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
+
 	NVIC_SetPriority(I2C1_EV_IRQn, 0);
 	NVIC_EnableIRQ(I2C1_EV_IRQn);
 
-	I2C_InitStruct.PeripheralMode	= LL_I2C_MODE_SMBUS_HOST;
-	I2C_InitStruct.Timing			= I2C_TIMING;
-	I2C_InitStruct.AnalogFilter		= LL_I2C_ANALOGFILTER_ENABLE;
-	I2C_InitStruct.DigitalFilter	= 0x00;
-	I2C_InitStruct.TypeAcknowledge	= LL_I2C_ACK;
-	I2C_InitStruct.OwnAddress1		= 0x00;
-	LL_I2C_Init(I2C1, &I2C_InitStruct);
+	NVIC_SetPriority(I2C1_ER_IRQn, 0);
+	NVIC_EnableIRQ(I2C1_ER_IRQn);
 
+/*
+	I2CCLK = HSI == 8MHz (RCC_CFG3.I2CxSW == 0 default) or SYSCLK (RCC_CFG3.I2CxSW == 1);
+	t_I2CCLK = 1 / I2CCLK;
+	t_PRESC = (PRESC + 1) * t_I2CCLK;
+	t_SCLDEL = (SCLDEL + 1) * t_PRESC;
+	t_SDADEL = SDADEL * t_PRESC;
+	t_SCLH = (SCLH + 1) * t_PRESC;
+	t_SCLL = (SCLL + 1) * t_PRESC;
+
+	I2CCLK == 8 MHz;
+	I2C Clock = 100 KHz;
+	PRESC = 0x1;
+	SCLDEL = 0x4;
+	SDADEL = 0x2;
+	SCLH = 0x0F;
+	SCLL = 0x13;
+
+	I2CCLK == 48 MHz;
+	I2C Clock = 100 KHz;
+	PRESC = 0xB;
+	SCLDEL = 0x4;
+	SDADEL = 0x2;
+	SCLH = 0x0F;
+	SCLL = 0x13;
+*/
+	LL_I2C_Disable(I2C1);
+
+	uint32_t PRESC = 0x1;
+	uint32_t SCLDEL = 0x4;
+	uint32_t SDADEL = 0x2;
+	uint32_t SCLH = 0x0F;
+	uint32_t SCLL = 0x13;
+
+	LL_I2C_SetTiming(I2C1, PRESC << 28 | SCLDEL << 20 | SDADEL << 16 | SCLH << 8 | SCLL);
+
+	LL_I2C_EnableIT_TX(I2C1);
+	LL_I2C_EnableIT_RX(I2C1);
+	LL_I2C_EnableIT_ADDR(I2C1);
+
+	/* Прерывания не генерировать,
+	 * это может нарушить ход синхронного приема.передачи
+	LL_I2C_EnableIT_NACK(I2C1);
+	LL_I2C_EnableIT_TC(I2C1);
+	 */
+
+	LL_I2C_EnableIT_STOP(I2C1);
+	LL_I2C_EnableIT_ERR(I2C1);
+
+	/* Уточнить, когда включать устройство относительно разрешения прерываний. */
 	LL_I2C_Enable(I2C1);
+}
+
+void I2C1_EV_IRQHandler(void)
+{
+	uint32_t i2c1_isr = READ_BIT(I2C1->ISR, 0xFFFFFFFF);
+	print("%s(): I2C1->ISR 0x%08X\r\n", __func__, i2c1_isr);
+
+	/* Снять все прерывания. */
+	WRITE_REG(I2C1->ICR, 0xFFFFFFFF);
+}
+
+void I2C1_ER_IRQHandler(void)
+{
+	uint32_t i2c1_isr = READ_BIT(I2C1->ISR, 0xFFFFFFFF);
+	print("%s(): I2C1->ISR 0x%08X\r\n", __func__, i2c1_isr);
+
+	/* Снять все прерывания. */
+	WRITE_REG(I2C1->ICR, 0xFFFFFFFF);
+}
+
+int32_t i2c_receive(uint8_t chip_addr, uint8_t reg_addr, uint8_t *buffer, size_t buffer_size)
+{
+	uint32_t guard_counter = GUARD_COUNTER_INIT;
+
+	LL_I2C_HandleTransfer(I2C1, chip_addr, LL_I2C_ADDRSLAVE_7BIT, 1, LL_I2C_MODE_SOFTEND, LL_I2C_GENERATE_START_WRITE);
+
+	if (LL_I2C_IsActiveFlag_NACK(I2C1) == 1)
+		return -1;
+
+	while (1) {
+
+		if (LL_I2C_IsActiveFlag_TXIS(I2C1) == 1)
+			LL_I2C_TransmitData8(I2C1, reg_addr);
+
+		if (LL_I2C_IsActiveFlag_NACK(I2C1) == 1)
+			return -2;
+
+		if (LL_I2C_IsActiveFlag_TC(I2C1) == 1)
+			break;
+		if (guard_counter-- == 0)
+			return -4;
+	}
+
+	LL_I2C_HandleTransfer(I2C1, chip_addr, LL_I2C_ADDRSLAVE_7BIT, buffer_size & 0xFF, LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_RESTART_7BIT_READ);
+
+	if (LL_I2C_IsActiveFlag_NACK(I2C1) == 1)
+		return -3;
+
+	size_t pos = 0;
+	guard_counter = GUARD_COUNTER_INIT;
+	while (1) {
+
+		if (LL_I2C_IsActiveFlag_RXNE(I2C1) == 1) {
+			buffer[pos] = LL_I2C_ReceiveData8(I2C1);
+			++pos;
+		}
+
+		if (LL_I2C_IsActiveFlag_TC(I2C1) == 1)
+			break;
+
+		if (guard_counter-- == 0)
+			return -4;
+	}
+	return pos;
 }
