2ed788a92f585936e5d0d47f60653b53b420e69b
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-02-13 17:44:19 +0300

    [C] i2c: чтение всех регистров INA3221 заменено на чтение регистров тока и напряжения, реалитзован механиз преобразования значений и вывода в консоль. Есть проблема с доступом по шине i2c после 120 итераций.

--- source/nucleo_main.c
+++ source/nucleo_main.c
@@ -36,42 +36,53 @@ void ds3231m_read_regs(void)
 
 }
 
-void ina3221_read_regs(void)
+void ina3221_print_voltage_current(void)
 {
+	uint8_t	ina3221_addr = 0x80;
 
-	print("%s()\r\n", __func__);
-
-	/* INA3221 */
-	uint8_t		ina3221_addr = 0x80;
-	uint8_t		reg_addr = 0x00; /* 0x00 - 0x11, 0xFE, 0xFF */
-	uint16_t	reg_val = 0xFFFF;
+	uint16_t v0 = 0, v1 = 0,v2 = 0;
+	uint16_t i0 = 0, i1 = 0,i2 = 0, I = 0;
 
 	int32_t ret_val = 0;
-	for (;reg_addr <= 0x11; ++reg_addr) {
-		ret_val = i2c_read(ina3221_addr, reg_addr, (uint8_t *)&reg_val, sizeof(reg_val));
-
-		if (ret_val != 2)
-			print("i2c_read(chip_addr=0x%02X, reg_addr=0x%02X, buffer_size=0x%02X)=%d\r\n", ina3221_addr, reg_addr, sizeof(reg_val), ret_val);
-		else
-			print("reg_addr=0x%02X, reg_val=0x%04X\r\n", reg_addr, swap_bytes_in_word(reg_val));
-		LL_mDelay(DELAY_50_MS);
+	ret_val = i2c_read(ina3221_addr, 0x1, (uint8_t *)&i0, sizeof(i0));
+	if (ret_val < 0) {
+		print("i2c_read_1 finished with error %d\r\n", ret_val);
+		return;
 	}
-	reg_addr = 0xFE;
-	ret_val = i2c_read(ina3221_addr, reg_addr, (uint8_t *)&reg_val, sizeof(reg_val));
+	ret_val = i2c_read(ina3221_addr, 0x2, (uint8_t *)&v0, sizeof(v0));
+	if (ret_val < 0) {
+		print("i2c_read_2 finished with error %d\r\n", ret_val);
+		return;
+	}
 
-	if (ret_val != 2)
-		print("i2c_read(chip_addr=0x%02X, reg_addr=0x%02X, buffer_size=0x%02X)=%d\r\n", ina3221_addr, reg_addr, sizeof(reg_val), ret_val);
-	else
-		print("reg_addr=0x%02X, reg_val=0x%04X\r\n", reg_addr, swap_bytes_in_word(reg_val));
+	ret_val = i2c_read(ina3221_addr, 0x3, (uint8_t *)&i1, sizeof(i1));
+	if (ret_val < 0) {
+		print("i2c_read_3 finished with error %d\r\n", ret_val);
+		return;
+	}
+	ret_val = i2c_read(ina3221_addr, 0x4, (uint8_t *)&v1, sizeof(v1));
+	if (ret_val < 0) {
+		print("i2c_read_4 finished with error %d\r\n", ret_val);
+		return;
+	}
 
-	reg_addr = 0xFF;
-	ret_val = i2c_read(ina3221_addr, reg_addr, (uint8_t *)&reg_val, sizeof(reg_val));
+	ret_val = i2c_read(ina3221_addr, 0x5, (uint8_t *)&i2, sizeof(i2));
+	if (ret_val < 0) {
+		print("i2c_read_5 finished with error %d\r\n", ret_val);
+		return;
+	}
+	ret_val = i2c_read(ina3221_addr, 0x6, (uint8_t *)&v2, sizeof(v2));
+	if (ret_val < 0) {
+		print("i2c_read_6 finished with error %d\r\n", ret_val);
+		return;
+	}
 
-	if (ret_val != 2)
-		print("i2c_read(chip_addr=0x%02X, reg_addr=0x%02X, buffer_size=0x%02X)=%d\r\n", ina3221_addr, reg_addr, sizeof(reg_val), ret_val);
-	else
-		print("reg_addr=0x%02X, reg_val=0x%04X\r\n", reg_addr, swap_bytes_in_word(reg_val));
+	/*i2c_read(ina3221_addr, 0xD, (uint8_t *)&I, sizeof(I));*/
 
+	print("Channel 0: voltage %dmV, current %dmA\r\n", swap_bytes_in_word(v0), (swap_bytes_in_word(i0) * 4) / 10);
+	print("Channel 1: voltage %dmV, current %dmA\r\n", swap_bytes_in_word(v1), (swap_bytes_in_word(i1) * 4) / 10);
+	print("Channel 2: voltage %dmV, current %dmA\r\n", swap_bytes_in_word(v2), (swap_bytes_in_word(i2) * 4) / 10);
+	/*print("Total current %dmA\r\n", (swap_bytes_in_word(I) * 4) / 10);*/
 }
 
 /**
@@ -93,11 +104,10 @@ int main(void)
 
 	i2c_init();
 
-	ina3221_read_regs();
-
 	uint32_t i = 0;
 	while (1) {
 		LL_mDelay(DELAY_500_MS * 2);
-		print("Main thread iteration %d\r", i++);
+		print("Main thread iteration %d\r\n", i++);
+		ina3221_print_voltage_current();
 	}
 }
