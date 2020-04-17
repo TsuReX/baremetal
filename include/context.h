2fbef29f7791c813b2ffb3be6da963f7c7f0a3c7
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2020-04-17 15:05:11 +0300

    [A] функциональность: добавлен механизм позволяющий избежать переполнения передающего кольцевого буфера, добавлен код для вывода содержимого стековых фреймов.

--- include/context.h
+++ include/context.h
@@ -8,6 +8,48 @@
 #ifndef INCLUDE_CONTEXT_H_
 #define INCLUDE_CONTEXT_H_
 
+#include <stddef.h>
+
+struct base_frame_t {
+	uint32_t	r0;
+	uint32_t	r1;
+	uint32_t	r2;
+	uint32_t	r3;
+	uint32_t	r12;
+	uint32_t	lr;
+	uint32_t	pc;
+	uint32_t	xpsr;
+};
+
+struct extended_frame_t {
+	uint32_t	r0;
+	uint32_t	r1;
+	uint32_t	r2;
+	uint32_t	r3;
+	uint32_t	r12;
+	uint32_t	lr;
+	uint32_t	pc;
+	uint32_t	xpsr;
+	uint32_t	s0;
+	uint32_t	s1;
+	uint32_t	s2;
+	uint32_t	s3;
+	uint32_t	s4;
+	uint32_t	s5;
+	uint32_t	s6;
+	uint32_t	s7;
+	uint32_t	s8;
+	uint32_t	s9;
+	uint32_t	s10;
+	uint32_t	s11;
+	uint32_t	s12;
+	uint32_t	s13;
+	uint32_t	s14;
+	uint32_t	s15;
+	uint32_t	fpscr;
+	uint32_t	reserved;
+};
+
 void nmi_trigger();
 
 #endif /* INCLUDE_CONTEXT_H_ */
--- source/context.c
+++ source/context.c
@@ -11,9 +11,41 @@ void nmi_trigger()
 	SCB->ICSR |= (1u << SCB_ICSR_NMIPENDSET_Pos);
 }
 
-#define EXC_RETURN_BASE_FRAME (1u << 4u)
-#define EXC_RETURN_THREAD_MODE (1u << 3u)
-#define EXC_RETURN_PROCESS_STACK (1u << 2u)
+#define EXC_RETURN_BASE_FRAME		(1u << 4u)
+#define EXC_RETURN_THREAD_MODE		(1u << 3u)
+#define EXC_RETURN_PROCESS_STACK	(1u << 2u)
+#define STACK_ALIGNED				(1u << 9u)
+
+static void base_frame_print(const struct base_frame_t *frame)
+{
+	print("Base frame:\n\r");
+
+	print("r0: 0x%08X\r\nr1: 0x%08X\r\nr2: 0x%08X\r\nr3: 0x%08X\r\nr12: 0x%08X\r\n",
+			frame->r0, frame->r1, frame->r2, frame->r3, frame->r12);
+
+	print("lr: 0x%08X\r\npc: 0x%08X\r\nxPSR: 0x%08X\r\n",
+			frame->lr, frame->pc, frame->xpsr);
+}
+
+static void extended_frame_print(const struct extended_frame_t *frame)
+{
+	print("Extended frame:\n\r");
+
+	print("r0: 0x%08X\r\nr1: 0x%08X\r\nr2: 0x%08X\r\nr3: 0x%08X\r\nr12: 0x%08X\r\n",
+			frame->r0, frame->r1, frame->r2, frame->r3, frame->r12);
+
+	print("lr: 0x%08X\r\npc: 0x%08X\r\nxPSR: 0x%08X\r\n",
+			frame->lr, frame->pc, frame->xpsr);
+
+	print("s0: 0x%08X\r\ns1: 0x%08X\r\ns2: 0x%08X\r\ns3: 0x%08X\r\n",
+			frame->s0, frame->s1, frame->s2, frame->s3);
+	print("s4: 0x%08X\r\ns5: 0x%08X\r\ns6: 0x%08X\r\ns7: 0x%08X\r\n",
+			frame->s4, frame->s5, frame->s6, frame->s7);
+	print("s8: 0x%08X\r\ns9: 0x%08X\r\ns10: 0x%08X\r\ns11: 0x%08X\r\n",
+			frame->s8, frame->s9, frame->s10, frame->s11);
+	print("s12: 0x%08X\r\ns13: 0x%08X\r\ns14: 0x%08X\r\ns15: 0x%08X\r\n",
+			frame->s12, frame->s13, frame->s14, frame->s15);
+}
 
 void context_parse(size_t frame_size, uint32_t exc_return, uint32_t sp)
 {
@@ -35,4 +67,14 @@ void context_parse(size_t frame_size, uint32_t exc_return, uint32_t sp)
 		print("PROCESS STACK\n\r");
 	else
 		print("MAIN STACK\n\r");
+
+	if ((exc_return & EXC_RETURN_BASE_FRAME) !=0)
+		base_frame_print((const struct base_frame_t*)sp);
+	else
+		extended_frame_print((const struct extended_frame_t*)sp);
+
+	if ((((const struct base_frame_t*)sp)->xpsr & STACK_ALIGNED) !=0 )
+		print("Stack pointer was ALIGNED\n\r");
+	else
+		print("Stack pointer was NOT ALIGNED\n\r");
 }
