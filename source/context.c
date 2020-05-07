3e09a10d996a9b1ec113c646334a99a031611755
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2020-05-07 20:15:04 +0300

    [F] вывод: исправлен формат вывода.

--- source/context.c
+++ source/context.c
@@ -20,10 +20,10 @@ static void base_frame_print(const struct base_frame_t *frame)
 {
 	print("Base frame:\n\r");
 
-	print("r0: 0x%08X\r\nr1: 0x%08X\r\nr2: 0x%08X\r\nr3: 0x%08X\r\nr12: 0x%08X\r\n",
+	print("r0: 0x%08lX\r\nr1: 0x%08lX\r\nr2: 0x%08lX\r\nr3: 0x%08lX\r\nr12: 0x%08lX\r\n",
 			frame->r0, frame->r1, frame->r2, frame->r3, frame->r12);
 
-	print("lr: 0x%08X\r\npc: 0x%08X\r\nxPSR: 0x%08X\r\n",
+	print("lr: 0x%08lX\r\npc: 0x%08lX\r\nxPSR: 0x%08lX\r\n",
 			frame->lr, frame->pc, frame->xpsr);
 }
 
@@ -31,25 +31,25 @@ static void extended_frame_print(const struct extended_frame_t *frame)
 {
 	print("Extended frame:\n\r");
 
-	print("r0: 0x%08X\r\nr1: 0x%08X\r\nr2: 0x%08X\r\nr3: 0x%08X\r\nr12: 0x%08X\r\n",
+	print("r0: 0x%08lX\r\nr1: 0x%08lX\r\nr2: 0x%08lX\r\nr3: 0x%08lX\r\nr12: 0x%08lX\r\n",
 			frame->r0, frame->r1, frame->r2, frame->r3, frame->r12);
 
-	print("lr: 0x%08X\r\npc: 0x%08X\r\nxPSR: 0x%08X\r\n",
+	print("lr: 0x%08lX\r\npc: 0x%08lX\r\nxPSR: 0x%08lX\r\n",
 			frame->lr, frame->pc, frame->xpsr);
 
-	print("s0: 0x%08X\r\ns1: 0x%08X\r\ns2: 0x%08X\r\ns3: 0x%08X\r\n",
+	print("s0: 0x%08lX\r\ns1: 0x%08lX\r\ns2: 0x%08lX\r\ns3: 0x%08lX\r\n",
 			frame->s0, frame->s1, frame->s2, frame->s3);
-	print("s4: 0x%08X\r\ns5: 0x%08X\r\ns6: 0x%08X\r\ns7: 0x%08X\r\n",
+	print("s4: 0x%08lX\r\ns5: 0x%08lX\r\ns6: 0x%08lX\r\ns7: 0x%08lX\r\n",
 			frame->s4, frame->s5, frame->s6, frame->s7);
-	print("s8: 0x%08X\r\ns9: 0x%08X\r\ns10: 0x%08X\r\ns11: 0x%08X\r\n",
+	print("s8: 0x%08lX\r\ns9: 0x%08lX\r\ns10: 0x%08lX\r\ns11: 0x%08lX\r\n",
 			frame->s8, frame->s9, frame->s10, frame->s11);
-	print("s12: 0x%08X\r\ns13: 0x%08X\r\ns14: 0x%08X\r\ns15: 0x%08X\r\n",
+	print("s12: 0x%08lX\r\ns13: 0x%08lX\r\ns14: 0x%08lX\r\ns15: 0x%08lX\r\n",
 			frame->s12, frame->s13, frame->s14, frame->s15);
 }
 
 void context_parse(size_t frame_size, uint32_t exc_return, uint32_t sp)
 {
-	print("frame_size: 0x%02X, lr: 0x%08X, sp: 0x%08X \r\n", frame_size, exc_return, sp);
+	print("frame_size: 0x%02X, lr: 0x%08lX, sp: 0x%08lX \r\n", frame_size, exc_return, sp);
 	print("Interrupt saved ");
 	if ((exc_return & EXC_RETURN_BASE_FRAME) !=0)
 		print("BASE FRAME ");
