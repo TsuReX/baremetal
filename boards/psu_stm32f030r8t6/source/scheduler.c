4ff626ecd83ac5ab38c8902520b3d8cbdeaf4960
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-08-31 14:51:36 +0300

    [C] psu_stm32f030r8t6: добавлен базовый проект новой платформы, проект собирается успешно.

---
+++ boards/psu_stm32f030r8t6/source/scheduler.c
@@ -0,0 +1,58 @@
+/**
+ * @file	src/scheduler.c
+ *
+ * @brief	Определения функций для работы с диспетчером на основе таймера TIM6.
+ *
+ * @author	Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
+ */
+
+#include "drivers.h"
+#include "scheduler.h"
+
+void scheduler_init(void)
+{
+
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM6);
+
+	LL_TIM_SetPrescaler(TIM6, 50);
+
+	LL_TIM_SetAutoReload(TIM6, 48000);
+
+	/* Set TIM_CR1_URS = 0 */
+	LL_TIM_SetUpdateSource(TIM6, LL_TIM_UPDATESOURCE_REGULAR);
+
+	/* Set TIM_CR1_UDIS = 0
+	 * By default TIM_CR1_UDIS is 0. */
+	LL_TIM_EnableUpdateEvent(TIM6);
+
+	LL_TIM_EnableIT_UPDATE(TIM6);
+
+	NVIC_SetPriority(TIM6_DAC1_IRQn, 0);
+
+	NVIC_EnableIRQ(TIM6_DAC1_IRQn);
+
+	LL_TIM_EnableCounter(TIM6);
+
+	LL_TIM_GenerateEvent_UPDATE(TIM6);
+}
+
+void scheduler_close(void)
+{
+	LL_TIM_DisableCounter(TIM6);
+	LL_TIM_DisableIT_UPDATE(TIM6);
+	NVIC_DisableIRQ(TIM6_DAC1_IRQn);
+}
+
+
+void __attribute__((weak)) scheduler_process(void)
+{
+}
+
+/*
+ *	Обработчик прерывания событий таймера TIM6.
+ */
+void TIM6_DAC1_IRQHandler(void)
+{
+	scheduler_process();
+	LL_TIM_ClearFlag_UPDATE(TIM6);
+}
---
+++ source/psu_main.c
@@ -0,0 +1,61 @@
+/**
+ * @file	src/psu_main.c
+ *
+ * @brief	Объявление точки вхождения в C-код.
+ *
+ * @author	Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
+ */
+
+//#include "i2c.h"
+#include "console.h"
+#include "config.h"
+//#include "scheduler.h"
+#include "drivers.h"
+//#include "ds3231m.h"
+//#include "ina3221.h"
+//#include "gpio.h"
+//#include "pwm.h"
+/**
+ * @brief	C-code entry point.
+ *
+ * @retval	0 in case of success, otherwise error code
+ */
+int main(void)
+{
+	/** Configure internal subsystems of SoC. */
+	soc_config();
+
+	/** Configure board's peripherals. */
+	board_config();
+
+//	console_init();
+
+//	scheduler_init();
+
+//	i2c_init();
+
+//	gpio_setup_it();
+
+//	pwm_init();
+
+	uint32_t duty = 10;
+	uint32_t up = 1;
+	while (1) {
+		LL_mDelay(DELAY_500_MS * 2);
+//		print("Duty cycle %ld\r\n", duty);
+//		ina3221_print_voltage_current();
+//		pwd_duty(duty);
+
+		if (up == 1)
+			duty += 10;
+
+		if (up == 0)
+			duty -= 10;
+
+		if (duty == 100)
+			up = 0;
+
+		if (duty == 10)
+			up = 1;
+	}
+}
