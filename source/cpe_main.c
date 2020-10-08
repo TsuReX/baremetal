3701d7459ffde5e200d90d2f994ca62489599a22
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-10-08 16:12:50 +0300

    [C] power seq: изменена последовательность запуска, добавлена возможность выключения, проект успешно собирается.

--- source/cpe_main.c
+++ source/cpe_main.c
@@ -13,7 +13,16 @@
 
 volatile uint32_t power_btn_pressed = 0;
 volatile uint32_t reset_btn_pressed = 0;
+
 volatile uint32_t timer_ticks = 0;
+
+#define GO_LOW		0
+#define GO_HIGH		1
+#define LOW			2
+#define HIGH		3
+
+volatile uint32_t SLP_S45_N = LOW;
+volatile uint32_t SLP_S3_N = LOW;
 
 void exti_0_1_irq_handler(void)
 {
@@ -23,7 +32,26 @@ void exti_0_1_irq_handler(void)
 
 void exti_2_3_irq_handler(void)
 {
-	d_print("%s()\r\n", __func__);
+//	d_print("%s()\r\n", __func__);
+
+	/* SLP_S45_N */
+	if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_2)){
+		if (SLP_S45_N == LOW){
+			SLP_S45_N = GO_HIGH;
+		} else {
+			SLP_S45_N = GO_LOW;
+		}
+	}
+
+	/* SLP_S3_N */
+	if (LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_3)){
+		if (SLP_S3_N == LOW){
+			SLP_S3_N = GO_HIGH;
+		} else {
+			SLP_S3_N = GO_LOW;
+		}
+	}
+
 	LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_3 | LL_EXTI_LINE_2);
 }
 
@@ -76,23 +104,34 @@ void exti_4_15_irq_handler(void)
 	LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_ALL_0_31 & ~(LL_EXTI_LINE_3 | LL_EXTI_LINE_2 | LL_EXTI_LINE_1 | LL_EXTI_LINE_0));
 }
 
-void gpio_irq_config(void)
+void gpio_irq_enable(void)
 {
 	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
+
+	/** SLP_S45_N */
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE2);
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_2);
+	LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_2);
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_2);
+
+	/** SLP_S3_N */
+	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE3);
+	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_3);
+	LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_3);
+	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_3);
 
 	/** FP_RST_BTN_N */
 	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);
 	LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_13, LL_GPIO_PULL_UP);
 	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_13);
 	LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_13);
-//	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_13);
 
 	/** FP_PWR_BTN_N */
 	LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE14);
 	LL_GPIO_SetPinPull(GPIOC, LL_GPIO_PIN_14, LL_GPIO_PULL_UP);
 	LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_14);
 	LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_14);
-//	LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_14);
+
 
 	NVIC_EnableIRQ(EXTI0_1_IRQn);
 	NVIC_SetPriority(EXTI0_1_IRQn, 0);
@@ -102,6 +141,37 @@ void gpio_irq_config(void)
 
 	NVIC_EnableIRQ(EXTI4_15_IRQn);
 	NVIC_SetPriority(EXTI4_15_IRQn, 0);
+}
+
+void gpio_irq_disable(void)
+{
+	NVIC_DisableIRQ(EXTI0_1_IRQn);
+
+	NVIC_DisableIRQ(EXTI2_3_IRQn);
+
+	NVIC_DisableIRQ(EXTI4_15_IRQn);
+
+	/** SLP_S45_N */
+	LL_EXTI_DisableIT_0_31(LL_EXTI_LINE_2);
+	LL_EXTI_DisableFallingTrig_0_31(LL_EXTI_LINE_2);
+	LL_EXTI_DisableRisingTrig_0_31(LL_EXTI_LINE_2);
+
+	/** SLP_S3_N */
+	LL_EXTI_DisableIT_0_31(LL_EXTI_LINE_3);
+	LL_EXTI_DisableFallingTrig_0_31(LL_EXTI_LINE_3);
+	LL_EXTI_DisableRisingTrig_0_31(LL_EXTI_LINE_3);
+
+	/** FP_RST_BTN_N */
+	LL_EXTI_DisableIT_0_31(LL_EXTI_LINE_13);
+	LL_EXTI_DisableFallingTrig_0_31(LL_EXTI_LINE_13);
+	LL_EXTI_DisableRisingTrig_0_31(LL_EXTI_LINE_13);
+
+	/** FP_PWR_BTN_N */
+	LL_EXTI_DisableIT_0_31(LL_EXTI_LINE_14);
+	LL_EXTI_DisableFallingTrig_0_31(LL_EXTI_LINE_14);
+	LL_EXTI_DisableRisingTrig_0_31(LL_EXTI_LINE_14);
+
+	LL_APB1_GRP2_DisableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
 }
 
 void scheduler_process(void)
@@ -310,8 +380,6 @@ int main(void)
 	/** Configure board's peripherals. */
 	board_config();
 
-	gpio_irq_config();
-
 	console_init();
 
 	scheduler_init();
@@ -340,42 +408,136 @@ int main(void)
 
 	LL_mDelay(200);
 
-	/* SLP_S45_N */
-	while (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_2) != 1);
-	d_print("SLP_S45_N is DEASSERTED\r\n");
-
-	vddq_vtt_on(1);
-	d_print("PSU VDDQ and VTT are ON\r\n");
-
-	/* SLP_S3_N */
-	while (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_3) != 1);
-	d_print("SLP_S3_N is DEASSERTED\r\n");
-
-	LL_mDelay(5);
-
-	vccref_on();
-	d_print("PSU VCCREF is ON\r\n");
-
-	LL_mDelay(5);
-
-	vccsram_on(1);
-	d_print("PSU VCCSRAM is ON\r\n");
-	vpp_on(1);
-	d_print("PSU VPP is ON\r\n");
-
-	LL_mDelay(5);
-
-	vccp_on(1);
-	d_print("PSU VCCP is ON\r\n");
-
-	/* COREPWEROR */
-	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_1);
-	d_print("COREPWEROR is DEASSERTED\r\n");
-
-	d_print("BIOS should start working\r\n");
+	gpio_irq_enable();
 
 	while (1) {
+
+//		if (power_btn_pushed == 1) {
+//			d_print("PWRBTN is pushed\r\n");
+//		}
+//
+//		if (power_btn_pulled == 1) {
+//			d_print("PWRBTN is pulled\r\n");
+//		}
+//
+//		if (reset_btn_pushed == 1) {
+//			d_print("RSTBTN is pushed\r\n");
+//		}
+//
+//		if (reset_btn_pulled == 1) {
+//			d_print("RSTBTN is pulled\r\n");
+//		}
+
+		if (SLP_S3_N == GO_LOW) {
+			d_print("SLP_S3_N is ASSERTED\r\n");
+
+			/* COREPWEROR */
+			LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_1);
+			d_print("COREPWEROK is ASSERTED\r\n");
+
+			LL_mDelay(1);
+
+			vccp_off(1);
+			d_print("PSU VCCP is OFF\r\n");
+
+			LL_mDelay(1);
+
+			vpp_off(1);
+			d_print("PSU VPP is OFF\r\n");
+
+			LL_mDelay(1);
+
+			vccsram_off(1);
+			d_print("PSU VCCSRAM is OFF\r\n");
+
+			LL_mDelay(1);
+
+			vccref_off();
+			d_print("PSU VCCREF is OFF\r\n");
+
+			SLP_S3_N = LOW;
+		}
+
+		if (SLP_S3_N == GO_HIGH && SLP_S45_N == HIGH) {
+			d_print("SLP_S3_N is DEASSERTED\r\n");
+
+			LL_mDelay(5);
+
+			vccref_on();
+			d_print("PSU VCCREF is ON\r\n");
+
+			LL_mDelay(5);
+
+			vccsram_on(1);
+			d_print("PSU VCCSRAM is ON\r\n");
+			vpp_on(1);
+			d_print("PSU VPP is ON\r\n");
+
+			LL_mDelay(5);
+
+			vccp_on(1);
+			d_print("PSU VCCP is ON\r\n");
+
+			/* COREPWEROR */
+			LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_1);
+			d_print("COREPWEROK is DEASSERTED\r\n");
+
+			SLP_S3_N = HIGH;
+		}
+
+		if (SLP_S45_N == GO_LOW && SLP_S3_N == LOW) {
+			d_print("SLP_S45_N is ASSERTED\r\n");
+
+			vddq_vtt_off(1);
+			d_print("PSU VDDQ and VTT are OFF\r\n");
+			SLP_S45_N = LOW;
+		}
+
+		if (SLP_S45_N == GO_HIGH) {
+			d_print("SLP_S45_N is DEASSERTED\r\n");
+
+			vddq_vtt_on(1);
+			d_print("PSU VDDQ and VTT are ON\r\n");
+
+			SLP_S45_N = HIGH;
+		}
+
 		LL_mDelay(1);
 	}
+
+
+//	/* SLP_S45_N */
+//	while (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_2) != 1);
+//	d_print("SLP_S45_N is DEASSERTED\r\n");
+//
+//	vddq_vtt_on(1);
+//	d_print("PSU VDDQ and VTT are ON\r\n");
+//
+//	/* SLP_S3_N */
+//	while (LL_GPIO_IsInputPinSet(GPIOC, LL_GPIO_PIN_3) != 1);
+//	d_print("SLP_S3_N is DEASSERTED\r\n");
+//
+//	LL_mDelay(5);
+//
+//	vccref_on();
+//	d_print("PSU VCCREF is ON\r\n");
+//
+//	LL_mDelay(5);
+//
+//	vccsram_on(1);
+//	d_print("PSU VCCSRAM is ON\r\n");
+//	vpp_on(1);
+//	d_print("PSU VPP is ON\r\n");
+//
+//	LL_mDelay(5);
+//
+//	vccp_on(1);
+//	d_print("PSU VCCP is ON\r\n");
+//
+//	/* COREPWEROR */
+//	LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_1);
+//	d_print("COREPWEROR is DEASSERTED\r\n");
+//
+//	d_print("BIOS should start working\r\n");
 
 }
