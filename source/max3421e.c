ddcb7d610d88b563011c67c5d781e5b424753251
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-12-02 19:04:40 +0300

    [A] hostusb_stm32f103c8t6 spi dma max3421e: взаимодествие с max переведено на использование dma, проект успешно собирается.

--- source/max3421e.c
+++ source/max3421e.c
@@ -32,11 +32,10 @@ uint8_t max3421e_usbirq_read(void)
 
 void max3421e_usbirq_write(uint8_t usbirq)
 {
-#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_WRUSBIRQ, usbirq};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 #else
-	uint8_t	buffer[2] = {SPI_CMD_WRUSBIRQ, usbirq};
 	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
 #endif
 }
@@ -58,11 +57,10 @@ uint8_t max3421e_usbctl_read(void)
 
 void max3421e_usbctl_write(uint8_t usbctl)
 {
-#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_WRUSBCTL, usbctl};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 #else
-	uint8_t	buffer[2] = {SPI_CMD_WRUSBCTL, usbctl};
 	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
 #endif
 }
@@ -84,11 +82,10 @@ uint8_t max3421e_cpuctl_read(void)
 
 void max3421e_cpuctl_write(uint8_t cpuctl)
 {
-#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_WRCPUCTL, cpuctl};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 #else
-	uint8_t	buffer[2] = {SPI_CMD_WRCPUCTL, cpuctl};
 	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
 #endif
 }
@@ -110,39 +107,62 @@ uint8_t max3421e_pinctl_read(void)
 
 void max3421e_pinctl_write(uint8_t pinctl)
 {
-#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_WRPINCTL, pinctl};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 #else
-	uint8_t	buffer[2] = {SPI_CMD_WRPINCTL, pinctl};
 	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
 #endif
 }
 
 uint8_t max3421e_iopins1_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDIOPINS1, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDIOPINS1;
+	uint8_t iopins1 = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&iopins1, sizeof(iopins1));
+	return iopins1;
+#endif
 }
 
 void max3421e_iopins1_write(uint8_t iopins1)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRIOPINS1, iopins1};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_iopins2_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDIOPINS2, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDIOPINS2;
+	uint8_t iopins2 = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&iopins2, sizeof(iopins2));
+	return iopins2;
+#endif
 }
 
 void max3421e_iopins2_write(uint8_t iopins1)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRIOPINS2, iopins1};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_rev_read(void)
@@ -152,7 +172,7 @@ uint8_t max3421e_rev_read(void)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
 #else
-	uint8_t	cmd = 0x05;
+	uint8_t	cmd = SPI_CMD_RDREV;
 	uint8_t revision = 0xFF;
 	spi_dma_data_send(&cmd, sizeof(cmd));
 	spi_dma_data_recv(&revision, sizeof(revision));
@@ -162,15 +182,27 @@ uint8_t max3421e_rev_read(void)
 
 uint8_t max3421e_hirq_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDHIRQ, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDHIRQ;
+	uint8_t hirq = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&hirq, sizeof(hirq));
+	return hirq;
+#endif
 }
 
 void max3421e_hirq_write(uint8_t hirq)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRHIRQ, hirq};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_mode_read(void)
@@ -201,122 +233,226 @@ void max3421e_mode_write(uint8_t mode)
 
 uint8_t max3421e_peraddr_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDPERADDR, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDPERADDR;
+	uint8_t peraddr = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&peraddr, sizeof(peraddr));
+	return peraddr;
+#endif
 }
 
 void max3421e_peraddr_write(uint8_t peraddr)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRPERADDR, peraddr};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_hctl_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDHCTL, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDHCTL;
+	uint8_t hctl = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&hctl, sizeof(hctl));
+	return hctl;
+#endif
 }
 
 void max3421e_hctl_write(uint8_t hctl)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRHCTL, hctl};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_hxfr_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDHXFR, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDHXFR;
+	uint8_t hxfr = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&hxfr, sizeof(hxfr));
+	return hxfr;
+#endif
 }
 
 void max3421e_hxfr_write(uint8_t hxfr)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRHXFR, hxfr};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_hrsl_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDHRSL, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDHRSL;
+	uint8_t hrsl = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&hrsl, sizeof(hrsl));
+	return hrsl;
+#endif
 }
 
 uint8_t max3421e_rcvfifo_write(uint8_t *src_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_WRRCVFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(src_buf, NULL, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(src_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 uint8_t max3421e_rcvfifo_read(uint8_t *dst_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_RDRCVFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(NULL, dst_buf, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(dst_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 uint8_t max3421e_sndfifo_write(uint8_t *src_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_WRSNDFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(src_buf, NULL, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(src_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 uint8_t max3421e_sndfifo_read(uint8_t *dst_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_RDSNDFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(NULL, dst_buf, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(dst_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 uint8_t max3421e_sudfifo_write(uint8_t *src_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_WRSUDFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(src_buf, NULL, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(src_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 uint8_t max3421e_sudfifo_read(uint8_t *dst_buf, size_t data_size)
 {
 	uint8_t	cmd = SPI_CMD_RDSUDFIFO;
+#if !defined(SPI_DMA)
 	spi_data_xfer(&cmd, NULL, 1);
 	size_t size = spi_data_xfer(NULL, dst_buf, data_size);
 	return size;
+#else
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(dst_buf, sizeof(data_size));
+	return data_size;
+#endif
 }
 
 void max3421e_rcvbc_write(uint8_t size)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRRCVBC, size};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_rcvbc_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDRCVBC, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDRCVBC;
+	uint8_t rcvdc = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&rcvdc, sizeof(rcvdc));
+	return rcvdc;
+#endif
 }
 
 void max3421e_sndbc_write(uint8_t size)
 {
 	uint8_t	buffer[2] = {SPI_CMD_WRSNDBC, size};
+#if !defined(SPI_DMA)
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
+#else
+	spi_dma_data_send(buffer, sizeof(buffer) / sizeof(buffer[0]));
+#endif
 }
 
 uint8_t max3421e_sndbc_read(void)
 {
+#if !defined(SPI_DMA)
 	uint8_t	buffer[2] = {SPI_CMD_RDSNDBC, 0x00};
 	spi_data_xfer(buffer, buffer, sizeof(buffer) / sizeof(buffer[0]));
 	return buffer[1];
+#else
+	uint8_t cmd = SPI_CMD_RDSNDBC;
+	uint8_t sndbc = 0xFF;
+	spi_dma_data_send(&cmd, sizeof(cmd));
+	spi_dma_data_recv(&sndbc, sizeof(sndbc));
+	return sndbc;
+#endif
 }
 
 /*********************************************************************************/
