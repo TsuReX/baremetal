386a4b112b7f8a3e59d87172e48d1f51faa9a9ff
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2020-12-24 16:13:43 +0300

    [C] psu_stm32f030r8t6: настроен алгоритм обработки оцифрованных данных, почищен файл console.c, проект успешно собирается.

--- source/psu_main.c
+++ source/psu_main.c
@@ -62,28 +62,25 @@ void adc_init()
 #define AMPL_VOLTS					(310)
 #define DIVIDED_AMPL_MILIVOLTS		(2370)
 
-#define VK_10			(((ADC_VREF_MILIVOLTS * 1000) / 4096) * ((AMPL_VOLT * 1000) / DIVIDED_AMPL_MILIVOLTS))
-//#define VK_16			(((ADC_VREF_MILIVOLTS << 10) >> 12) * ((AMPL_VOLTS << 10) / DIVIDED_AMPL_MILIVOLTS)))
+#define VK_10			(((ADC_VREF_MILIVOLTS * 1000) / 4096) * ((AMPL_VOLTS * 1000) / DIVIDED_AMPL_MILIVOLTS))
 #define VK_16			(((ADC_VREF_MILIVOLTS >> 2) * ((AMPL_VOLTS << 10) / DIVIDED_AMPL_MILIVOLTS)))
 #define CONVERT_10(X)	(((uint32_t)X * VK_10) / 1000000)
 #define CONVERT_16(X)	(((uint32_t)X * VK_16) >> 20)
 
 void adc_start_convertion(void)
 {
-
 	LL_ADC_ClearFlag_ADRDY(ADC1);
+
 	uint16_t enc_vac = 0;
 	uint16_t enc_vpfc = 0;
 	uint32_t vac = 0;
 	uint32_t vpfc = 0;
 	uint32_t max_vac = 0;
 	uint32_t max_vpfc = 0;
-
 	size_t sample_num = 0;
 	const size_t sub_sample_count = 128;
 	size_t sub_sample_num = 0;
 	const size_t sample_count = 16;
-//	uint32_t samples_full = 0;
 	uint32_t vacs[sample_count];
 	uint32_t vpfcs[sample_count];
 
@@ -92,51 +89,64 @@ void adc_start_convertion(void)
 
 	while (1) {
 		LL_ADC_REG_StartConversion(ADC1);
+
 		while (!LL_ADC_IsActiveFlag_EOC(ADC1));
 		enc_vac = LL_ADC_REG_ReadConversionData12(ADC1);
+		udelay(1);
 		LL_ADC_ClearFlag_EOC(ADC1);
 
 		LL_ADC_REG_StartConversion(ADC1);
 		while (!LL_ADC_IsActiveFlag_EOC(ADC1));
 		enc_vpfc = LL_ADC_REG_ReadConversionData12(ADC1);
+		udelay(1);
 		LL_ADC_ClearFlag_EOC(ADC1);
 
 		if (LL_ADC_IsActiveFlag_EOS(ADC1)) {
-			vac = CONVERT_16(enc_vac);
-			vpfc = CONVERT_16(enc_vpfc);
-//			printk(INFO, "VAC: %ld, VPFC: %ld\r\n", vac, vpfc);
+			LL_ADC_ClearFlag_EOS(ADC1);
 		}
-		LL_ADC_ClearFlag_EOS(ADC1);
+		vac = CONVERT_10(enc_vac);
+		vpfc = CONVERT_16(enc_vpfc);
 
 		udelay(100);
 
 		if (sub_sample_num++ == sub_sample_count) {
 			vacs[sample_num] = max_vac;
 			vpfcs[sample_num] = max_vpfc;
+			max_vac = 0;
+			max_vpfc = 0;
 			sub_sample_num = 0;
-//			if (samples_full == 0 & sample_num == (sample_count - 1))
-//				samples_full = 1;
 			sample_num++;
 			sample_num %= sample_count;
+
+			uint32_t sum_vac = 0;
+			uint32_t sum_vpfc = 0;
+			uint32_t avg_vac = 0;
+			uint32_t avg_vpfc = 0;
+
+			size_t i = 0;
+			for (; i < sample_count; i++) {
+				sum_vac += vacs[i];
+				sum_vpfc += vpfcs[i];
+			}
+
+			avg_vac = sum_vac >> 4;
+			avg_vpfc = sum_vpfc >> 4;
+
+			printk(INFO, "VAC: %ld, VPFC: %ld\r\n", avg_vac, avg_vpfc);
+//			printk(INFO, "MAXVAC: %ld, MAXVPFC: %ld\r\n", max_vac, max_vpfc);
+//			printk(INFO, "ENCVAC: %d, ENCVPFC: %d\r\n", enc_vac, enc_vpfc);
+
+//			console_write((uint8_t *)&avg_vac, sizeof(avg_vac), 1000);
+//			console_write((uint8_t *)&avg_vpfc, sizeof(avg_vpfc), 1000);
+
 		} else {
 			if (vac > max_vac)
 				max_vac = vac;
 			if (vpfc > max_vpfc)
 				max_vpfc = vpfc;
 		}
-
-		uint32_t sum_vac = 0;
-		uint32_t sum_vpfc = 0;
-		size_t i = 0;
-		for (; i < sample_count; i++) {
-			sum_vac += vacs[i];
-			sum_vpfc += vpfcs[i];
-		}
-		uint32_t avg_vac = sum_vac /* / sample_count */ >> 4;
-		uint32_t avg_vpfc = sum_vpfc /* / sample_count */ >> 4;
-
-		console_write((uint8_t *)&avg_vac, sizeof(avg_vac), 1000);
-		console_write((uint8_t *)&avg_vpfc, sizeof(avg_vpfc), 1000);
+		/* TODO: Remove. Used for debugging */
+		LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_1);
 	}
 }
 
@@ -154,16 +164,17 @@ int main(void)
 	board_init();
 
 	console_init();
+
 	adc_init();
 
-//	adc_start_convertion();
+	adc_start_convertion();
 
 	while (1) {
 		udelay(500000);
-		LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_8);
+		LL_GPIO_SetOutputPin(GPIOF, LL_GPIO_PIN_1);
 		LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_1);
 		udelay(500000);
-		LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_8);
+		LL_GPIO_ResetOutputPin(GPIOF, LL_GPIO_PIN_1);
 		LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_1);
 	}
 }
