4612ee71bc22fc5420a6a392c6613bafbbb1ab7e
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2021-02-01 13:31:03 +0300

    [A] i2c, max7219cwg: реализован код вывода данных на 8ми элементный сегментный дисплей, проект успешно собирается.

--- source/stm_bluepill_main.c
+++ source/stm_bluepill_main.c
@@ -8,17 +8,88 @@
 #include "debug.h"
 #include "spi_flash.h"
 
-int main(void)
+void max7219_check(void)
 {
-	soc_init();
+	mdelay(1000);
+	uint16_t	cmd = 0x0;
 
-	board_init();
+	cmd = 0x0701;
+	spi_chip_activate(FLASH_CHANNEL);
+	spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+	spi_chip_deactivate(FLASH_CHANNEL);
 
-	console_init();
+	cmd = 0x0F0A;
+	spi_chip_activate(FLASH_CHANNEL);
+	spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+	spi_chip_deactivate(FLASH_CHANNEL);
 
-	spi_init();
+	cmd = 0xFF09;
+	spi_chip_activate(FLASH_CHANNEL);
+	spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+	spi_chip_deactivate(FLASH_CHANNEL);
 
+	cmd = 0x0F0B;
+	spi_chip_activate(FLASH_CHANNEL);
+	spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+	spi_chip_deactivate(FLASH_CHANNEL);
 
+	cmd = 0x010C;
+	spi_chip_activate(FLASH_CHANNEL);
+	spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+	spi_chip_deactivate(FLASH_CHANNEL);
+
+//	mdelay(2000);
+	uint8_t val = 0;
+	while (1) {
+		cmd = (val << 8) | 0x1;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x2;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x3;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x4;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x5;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x6;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x7;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		cmd = (val << 8) | 0x8;
+		spi_chip_activate(FLASH_CHANNEL);
+		spi_dma_data_send((uint8_t*)&cmd, sizeof(cmd));
+		spi_chip_deactivate(FLASH_CHANNEL);
+
+		val = (val + 1) % 9;
+
+		mdelay(500);
+	}
+
+}
+
+void sfdp_check(void)
+{
 	uint8_t sfdp[256];
 	size_t sfdp_size = 16;
 	memset(sfdp, 0xFF, sizeof(sfdp));
@@ -47,6 +118,24 @@ int main(void)
 		LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_13);
 		__WFI();
 	}
+}
+#define FUNC 1
+int main(void)
+{
+	soc_init();
+
+	board_init();
+
+	console_init();
+
+	spi_init();
+
+#if (FUNC == 0)
+	sfdp_check();
+#elif (FUNC == 1)
+	max7219_check();
+	__WFI();
+#endif
 
 	return 0;
 }
