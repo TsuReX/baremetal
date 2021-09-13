5e3b752a4d747b0f468eb70372077187d5479a54
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2021-09-13 13:58:25 +0300

    [C] реорганизованы сборочные основной и вспомогательный скрипты проект hostusb_mdr32f9, проект успешно собирается.

--- boards/bluepill_stm32f103c8t6/building.cmake
+++ boards/bluepill_stm32f103c8t6/building.cmake
@@ -20,48 +20,50 @@ set(BOARD_TYPE_STATUS "SET")
 ## Подключение файло исходных кодов и заголовков
 set(BOARD_SRC_PATH		"${CMAKE_CURRENT_SOURCE_DIR}/boards/bluepill_stm32f103c8t6")
 
-set(INCLUDE		"${INCLUDE}"
+set(INCLUDE			"${INCLUDE}"
 
-				"${CMAKE_CURRENT_SOURCE_DIR}/include"
-				
-				"${BOARD_SRC_PATH}/include"
-				
-				"${BOARD_SRC_PATH}/usblib/include"
+					"${CMAKE_CURRENT_SOURCE_DIR}/include"
+
+					"${BOARD_SRC_PATH}/include"
+
+					"${BOARD_SRC_PATH}/usblib/include"
 			)
 
-set(MAIN_SOURCES		"${MAIN_SOURCES}"
+set(MAIN_SOURCES	"${MAIN_SOURCES}"
 
-				"${CMAKE_CURRENT_SOURCE_DIR}/source/stm_bluepill_main.c"
-				"${CMAKE_CURRENT_SOURCE_DIR}/source/debug.c"
-				"${CMAKE_CURRENT_SOURCE_DIR}/source/spi_flash.c"
-				"${CMAKE_CURRENT_SOURCE_DIR}/source/time.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/source/stm_bluepill_main.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/source/debug.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/source/spi_flash.c"
+					"${CMAKE_CURRENT_SOURCE_DIR}/source/time.c"
 
-				"${BOARD_SRC_PATH}/source/startup_stm32f103xb.c"
-				"${BOARD_SRC_PATH}/source/system_stm32f103xb.c"
-				"${BOARD_SRC_PATH}/source/init.c"
-				"${BOARD_SRC_PATH}/source/platform.c"
-				"${BOARD_SRC_PATH}/source/console.c"
-				"${BOARD_SRC_PATH}/source/scheduler.c"
-				"${BOARD_SRC_PATH}/source/spi.c"
+					"${BOARD_SRC_PATH}/source/startup_stm32f103xb.c"
+					"${BOARD_SRC_PATH}/source/system_stm32f103xb.c"
+					"${BOARD_SRC_PATH}/source/init.c"
+					"${BOARD_SRC_PATH}/source/platform.c"
+					"${BOARD_SRC_PATH}/source/console.c"
+					"${BOARD_SRC_PATH}/source/scheduler.c"
+					"${BOARD_SRC_PATH}/source/spi.c"
 			)
 
-set(INCLUDE		"${INCLUDE}"
-				"${CMAKE_CURRENT_SOURCE_DIR}/base/core/include"
+set(INCLUDE			"${INCLUDE}"
 
-				"${CMAKE_CURRENT_SOURCE_DIR}/base/device/st/stm32f1xx/include"
+					"${CMAKE_CURRENT_SOURCE_DIR}/base/core/include"
 
-				"${CMAKE_CURRENT_SOURCE_DIR}/base/driver/st/stm32f1xx/include"
+					"${CMAKE_CURRENT_SOURCE_DIR}/base/device/st/stm32f1xx/include"
+
+					"${CMAKE_CURRENT_SOURCE_DIR}/base/driver/st/stm32f1xx/include"
 			)
 
 set(DRV_SOURCES_PATH 	"${CMAKE_CURRENT_SOURCE_DIR}/base/driver/st/stm32f1xx/source")
 
 set(MAIN_SOURCES 	"${MAIN_SOURCES}"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_i2c.c"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_gpio.c"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_usart.c"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_dma.c"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_tim.c"
-				"${DRV_SOURCES_PATH}/stm32f1xx_ll_utils.c"
+
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_i2c.c"
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_gpio.c"
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_usart.c"
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_dma.c"
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_tim.c"
+					"${DRV_SOURCES_PATH}/stm32f1xx_ll_utils.c"
 			)
 
 #######################################################################
