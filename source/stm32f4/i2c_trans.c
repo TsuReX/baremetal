934cbb51372a0810755c78ae6458f6ab8424551b
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2021-09-21 00:50:07 +0300

    [C] исправлена ошибка в функции i2c_write(), добавлены функции настройки и чтения данных из акселерометра, гироскопа, и магнетометра, проект успешно собирается.

--- source/stm32f4/i2c_trans.c
+++ source/stm32f4/i2c_trans.c
@@ -1,6 +1,7 @@
 #include "i2c_trans.h"
+#include "debug.h"
 
-#define GUARD_COUNTER_INIT	1000
+#define GUARD_COUNTER_INIT	10000
 #define I2C_REQUEST_WRITE	0x00
 #define I2C_REQUEST_READ	0x01
 
@@ -116,59 +117,44 @@ int32_t i2c_read(I2C_TypeDef *i2c_bus, uint8_t chip_addr, uint8_t reg_addr, uint
 int32_t i2c_write(I2C_TypeDef *i2c_bus, uint8_t chip_addr, uint8_t reg_addr, uint8_t *buffer, size_t buffer_size)
 {
 	uint32_t guard_counter = GUARD_COUNTER_INIT;
-
-	/* (1) Prepare acknowledge for Master data reception ************************/
+	LL_I2C_DisableBitPOS(i2c_bus);
 	LL_I2C_AcknowledgeNextData(i2c_bus, LL_I2C_ACK);
 //	printk(DEBUG, "i2c_write 1\r\n");
-	/* (2) Initiate a Start condition to the Slave device ***********************/
-	/* Master Generate Start condition */
 	LL_I2C_GenerateStartCondition(i2c_bus);
 //	printk(DEBUG, "i2c_write 2\r\n");
-	/* (3) Loop until Start Bit transmitted (SB flag raised) ********************/
-//	printk(DEBUG, "i2c_bus->SR1: 0x%02lX\r\n", READ_BIT(i2c_bus->SR1, 0xFF));
-//	printk(DEBUG, "i2c_bus->SR2: 0x%02lX\r\n", READ_BIT(i2c_bus->SR2, 0xFF));
-	/* Loop until SB flag is raised  */
 	while(!LL_I2C_IsActiveFlag_SB(i2c_bus)) {
 		if (guard_counter-- == 0)
 			return -1;
 	}
 //	printk(DEBUG, "i2c_write 3\r\n");
-	/* (4) Send Slave address with a 7-Bit SLAVE_OWN_ADDRESS for a write request */
 	LL_I2C_TransmitData8(i2c_bus, chip_addr | I2C_REQUEST_WRITE);
 //	printk(DEBUG, "i2c_write 4\r\n");
-	/* (5) Loop until Address Acknowledgement received (ADDR flag raised) *******/
 
 	guard_counter = GUARD_COUNTER_INIT;
-	/* Loop until ADDR flag is raised  */
 	while(!LL_I2C_IsActiveFlag_ADDR(i2c_bus)) {
 		if (guard_counter-- == 0)
 			return -2;
 	}
 //	printk(DEBUG, "i2c_write 5\r\n");
-	/* (6) Clear ADDR flag and loop until end of transfer (ubNbDataToTransmit == 0) */
-
-	/* Clear ADDR flag value in ISR register */
 	LL_I2C_ClearFlag_ADDR(i2c_bus);
 //	printk(DEBUG, "i2c_write 6\r\n");
+	LL_I2C_TransmitData8(i2c_bus, reg_addr);
+	while(!LL_I2C_IsActiveFlag_TXE(i2c_bus)) {
+		if (guard_counter-- == 0)
+					return -3;
+	}
+
 	guard_counter = GUARD_COUNTER_INIT;
-	/* Loop until TXE flag is raised  */
 	while(buffer_size > 0) {
-		/* (6.1) Transmit data (TXE flag raised) **********************************/
 
-		/* Check TXE flag value in ISR register */
 		if(LL_I2C_IsActiveFlag_TXE(i2c_bus)) {
-			/* Write data in Transmit Data register.
-			TXE flag is cleared by writing data in TXDR register */
 			LL_I2C_TransmitData8(i2c_bus, (*buffer++));
-
 			buffer_size--;
 		}
 		if (guard_counter-- == 0)
-			return -3;
+			return -4;
 	}
 //	printk(DEBUG, "i2c_write 7\r\n");
-	/* (7) End of tranfer, Data consistency are checking into Slave process *****/
-	/* Generate Stop condition */
 	LL_I2C_GenerateStopCondition(i2c_bus);
 //	printk(DEBUG, "i2c_write 8\r\n");
 	return 0;
