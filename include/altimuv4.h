066961217467a40850392cc38089e013b6f2e743
Vasily Yurchenko <vasily.v.yurchenko@yandex.ru>
2021-09-21 12:22:02 +0300

    [C] код инициализации и получения данных от гироскопа, акселерометра и магнетометра, проект успешно собирается.

---
+++ include/altimuv4.h
@@ -0,0 +1,18 @@
+/*
+ * altimuv4.h
+ *
+ *  Created on: Sep 21, 2021
+ *      Author: user
+ */
+
+#ifndef INCLUDE_ALTIMUV4_H_
+#define INCLUDE_ALTIMUV4_H_
+
+void gyro_init();
+void gyro_read();
+void accel_init();
+void magnet_init();
+void accel_read();
+void magnet_read();
+
+#endif /* INCLUDE_ALTIMUV4_H_ */
