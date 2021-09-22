0bdb15523883adc96ee17149885a36a5a13678a0
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2021-09-22 10:20:51 +0300

    [C] доработана функция i2c_write(), уменьшина скорость шины i2c до 100КГц, дорабатывается код управления микросхемами D85-D88, проект успешно собирается.

--- source/stm32f4/i2c_trans.c
+++ source/stm32f4/i2c_trans.c
@@ -107,21 +107,27 @@ int32_t i2c_write(I2C_TypeDef *i2c_bus, uint8_t chip_addr, uint8_t reg_addr, uin
 	LL_I2C_AcknowledgeNextData(i2c_bus, LL_I2C_ACK);
 	LL_I2C_GenerateStartCondition(i2c_bus);
 	while(!LL_I2C_IsActiveFlag_SB(i2c_bus)) {
-		if (guard_counter-- == 0)
+		if (guard_counter-- == 0) {
+			LL_I2C_GenerateStopCondition(i2c_bus);
 			return -1;
+		}
 	}
 	LL_I2C_TransmitData8(i2c_bus, chip_addr | I2C_REQUEST_WRITE);
 
 	guard_counter = GUARD_COUNTER_INIT;
 	while(!LL_I2C_IsActiveFlag_ADDR(i2c_bus)) {
-		if (guard_counter-- == 0)
+		if (guard_counter-- == 0) {
+			LL_I2C_GenerateStopCondition(i2c_bus);
 			return -2;
+		}
 	}
 	LL_I2C_ClearFlag_ADDR(i2c_bus);
 	LL_I2C_TransmitData8(i2c_bus, reg_addr);
 	while(!LL_I2C_IsActiveFlag_TXE(i2c_bus)) {
-		if (guard_counter-- == 0)
-					return -3;
+		if (guard_counter-- == 0) {
+			LL_I2C_GenerateStopCondition(i2c_bus);
+			return -3;
+		}
 	}
 
 	guard_counter = GUARD_COUNTER_INIT;
@@ -130,8 +136,10 @@ int32_t i2c_write(I2C_TypeDef *i2c_bus, uint8_t chip_addr, uint8_t reg_addr, uin
 		LL_I2C_TransmitData8(i2c_bus, (*buffer++));
 		buffer_size--;
 		while(!LL_I2C_IsActiveFlag_TXE(i2c_bus)) {
-			if (guard_counter-- == 0)
-						return -4;
+			if (guard_counter-- == 0) {
+				LL_I2C_GenerateStopCondition(i2c_bus);
+				return -4;
+			}
 		}
 	}
 	LL_I2C_GenerateStopCondition(i2c_bus);
