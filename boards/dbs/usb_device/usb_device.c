9d6716193c505a90b518ff5c798ecbf49c11618b
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2021-09-27 23:28:54 +0300

    [A] добавлен отладочный вывод для определения ошибки инициализации USB, проект успешно собирается.

--- boards/dbs/usb_device/usb_device.c
+++ boards/dbs/usb_device/usb_device.c
@@ -13,28 +13,29 @@ uint32_t usb_inited = 0;
   * Init USB device Library, add supported class and start the library
   * @retval None
   */
-void usb_init(void)
+int32_t usb_init(void)
 {
 	/* Init Device Library, add supported class and start the library. */
 
 	if (usb_device_init(&usb_device, &device_descriptors, DEVICE_FS) != USBD_OK) {
-		printk(DEBUG, "USBD_Init error\r\n");
-		return;
+		printk(DEBUG, "usb_device_init error\r\n");
+		return -1;
 	}
 
 	if (usb_device_register_class(&usb_device, &usb_communication_device_class) != USBD_OK) {
-		printk(DEBUG, "USBD_RegisterClass error\r\n");
-		return;
+		printk(DEBUG, "usb_device_register_class error\r\n");
+		return -2;
 	}
 
 	if (usb_device_register_cdc_operations(&usb_device, &cdc_operations) != USBD_OK) {
-		printk(DEBUG, "USBD_CDC_RegisterInterface error\r\n");
-		return;
+		printk(DEBUG, "usb_device_register_cdc_operations error\r\n");
+		return -3;
 	}
 
 	if (usb_device_start(&usb_device) != USBD_OK) {
-		printk(DEBUG, "USBD_Start error\r\n");
-		return;
+		printk(DEBUG, "usb_device_start error\r\n");
+		return -4;
 	}
 	usb_inited = 1;
+	return 0;
 }
