c9f020f6add5591aa4d66e5294e041cea3dc1b57
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2024-06-27 14:28:34 +0000

    changes: 	Mechanism of passthrough of all cache levels (CLIDR_EL1) was tested and fixed; 	Auxiliary scripts were modified.

--- boards/imx8m/.gdbinit
+++ boards/imx8m/.gdbinit
@@ -1,10 +1,10 @@
 target remote localhost:1234
 layout asm
 layout regs
-layout split
 focus cmd
 symbol-file imx8m.elf
 set architecture aarch64
 b _start
+b clear_cache
 info reg pc
 list
--- boards/imx8m/run-autobuild.sh
+++ boards/imx8m/run-autobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while inotifywait -rqe modify ${PWD}/../boards/imx8m/source; do
-    echo "---> New build"
+    echo "---> New build: `date`"
     cmake --build .
     sleep 2
     echo ""
