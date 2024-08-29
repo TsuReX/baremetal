dad32e88759163a4191e6e1b49ab4f5b037fd050
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2024-08-29 15:01:09 +0000

    changes: 	p2sb_init function was implemented to configure pcr access.

--- boards/x86/source/ast2500.c
+++ boards/x86/source/ast2500.c
@@ -1,12 +1,58 @@
 #include <ast2500.h>
 #include <io.h>
+/*
+static unsigned int pci_io_encode_addr(unsigned int dev, unsigned short reg) {
+    
+    unsigned int addr = 1U << 31;
+
+    addr |= dev >> 4;
+    addr |= reg & 0xfc;
+
+    if (CONFIG(PCI_IO_CFG_EXT))
+	addr |= (reg & 0xf00) << 16;
+
+    return addr;
+}
+*/
+static void p2sb_init(void) {
+
+#define	PCH_DEV_SLOT_LPC			0x1f
+#define	PCI_DEV(_bus, _dev, _fn)	(0x80000000 | (unsigned int)(_bus << 16) | (unsigned int)(_dev << 11) | (unsigned int)(_fn << 8))
+#define	_PCH_DEV(slot, func)		PCI_DEV(0, PCH_DEV_SLOT_ ## slot, func)
+#define	PCH_DEV_P2SB				_PCH_DEV(LPC, 1)
+
+#define	PCI_COMMAND				0x04	/* 16 bits */
+#define	PCI_COMMAND_MEMORY		0x02	/* Enable response in Memory space */
+#define	PCI_BASE_ADDRESS_0		0x10	/* 32 bits */
+#define CONFIG_PCR_BASE_ADDRESS	0xFD000000
+
+//PCI_DEV(0, 0x1f, 1) == PCH_DEV_P2SB
+
+    /* Set up P2SB BAR. This is needed for PCR to work */
+//	PCI_CF8_ADDR(Bus, Dev, Func, Off)
+//	PCI_CF8_ADDR(0, 0x1F, 1, Off)
+    //unsigned char p2sb_cmd = pci_s_read_config8(PCH_DEV_P2SB, PCI_COMMAND);
+    outl(PCH_DEV_P2SB, R_PCH_IOPORT_PCI_INDEX);
+    unsigned char p2sb_cmd = inb(R_PCH_IOPORT_PCI_DATA);
+    
+    //pci_s_write_config8(PCH_DEV_P2SB, PCI_COMMAND, p2sb_cmd | PCI_COMMAND_MEMORY);
+    outl(PCI_CF8_ADDR(0, 0x1F, 1, PCI_COMMAND), R_PCH_IOPORT_PCI_INDEX);
+    outb(p2sb_cmd | PCI_COMMAND_MEMORY, R_PCH_IOPORT_PCI_DATA);
+    
+    //pci_s_write_config32(PCH_DEV_P2SB, PCI_BASE_ADDRESS_0, CONFIG_PCR_BASE_ADDRESS);
+    outl(PCI_CF8_ADDR(0, 0x1F, 1, PCI_BASE_ADDRESS_0), R_PCH_IOPORT_PCI_INDEX);
+    outl(CONFIG_PCR_BASE_ADDRESS, R_PCH_IOPORT_PCI_DATA);
+}
 
 void sio_ast2500_init (unsigned int base) {
 
     unsigned short  lpciod;
     unsigned short  lpcioe;
 
+    p2sb_init();
+
     // Enable LPC decode
+    // Set COMA/COMB base
     lpciod =  (V_LPC_CFG_IOD_COMA_3F8 << N_LPC_CFG_IOD_COMA) | (V_LPC_CFG_IOD_COMB_2F8 << N_LPC_CFG_IOD_COMB);
     lpcioe =  (B_LPC_CFG_IOE_SE | B_LPC_CFG_IOE_CBE | B_LPC_CFG_IOE_CAE);
 
