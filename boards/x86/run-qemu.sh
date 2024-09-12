d47e27bd852e6eda746e81e96d6caf9186810ec7
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2024-09-12 02:58:30 +0000

    changes: 	Qemu invocation script modifies bin_table in bios binary before execution to place address' of FSP-T Microcode Update regions.

--- boards/x86/run-qemu.sh
+++ boards/x86/run-qemu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO: Check presence of $1 argument and file existance and execute qemu with appropriate parameters
 
@@ -27,22 +27,54 @@
 
 # This value contains address where FSP-T is placed
 FSP_T_ADDR=0x00010000
-printf "0x%08X\n" $FSP_T_ADDR
+printf "FSP_T_ADDR: 0x%08X\n" $FSP_T_ADDR
 FSP_T_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~FSP_T_ADDR) ))
-printf "0x%08X\n" $FSP_T_ADDR_COMPLEMENT
+printf "FSP_T_ADDR_COMPLEMENT: 0x%08X\n" $FSP_T_ADDR_COMPLEMENT
+
+# This value contains address where Microcode Update is placed
+MICRO_UPD_ADDR=0x00020000
+printf "MICRO_UPD_ADDR: 0x%08X\n" $MICRO_UPD_ADDR
+MICRO_UPD_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~MICRO_UPD_ADDR) ))
+printf "MICRO_UPD_ADDR_COMPLEMENT: 0x%08X\n" $MICRO_UPD_ADDR_COMPLEMENT
 
 # This value contains path and name of being loaded FSP-T file
 FSP_T_FILE=$1
+
+# This value contains path and name of being loaded Microcode Update file
+MICRO_UPD_FILE=$2
+
 # This value contains address of FSP-T address
-FSP_T_ADDR_STORAGE=0x00080000
+BIN_TABLE=$(((1 << 32) - 0x40))
+printf "BIN_TABLE: 0x%08X\n" $BIN_TABLE
+
 # This value contains path and name of being loaded BIOS file
 BIOS_FILE=x86.bin
+
+BIOS_SIZE=`stat --format="%s" $BIOS_FILE`
+printf "BIOS_SIZE: 0x%08X\n" $BIOS_SIZE
+
+BIOS_ADDR=$(( (1<<32) - BIOS_SIZE ))
+printf "BIOS_ADDR: 0x%08X\n" $BIOS_ADDR
+
+to_little_ending() {
+    V=`printf "%08x" $1`
+    V_LE=0x`echo ${V:6:2}${V:4:2}${V:2:2}${V:0:2}`
+    echo $V_LE
+}
+
+ADDR=$((0x3000 - 0x40 + 0x0)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $FSP_T_ADDR)`; echo $VAR | xxd -r - $BIOS_FILE
+ADDR=$((0x3000 - 0x40 + 0x4)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $FSP_T_ADDR_COMPLEMENT)`; echo $VAR | xxd -r - $BIOS_FILE
+
+ADDR=$((0x3000 - 0x40 + 0x8)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $MICRO_UPD_ADDR)`; echo $VAR | xxd -r - $BIOS_FILE
+ADDR=$((0x3000 - 0x40 + 0xC)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $MICRO_UPD_ADDR_COMPLEMENT)`; echo $VAR | xxd -r - $BIOS_FILE
 
 qemu-system-i386 \
 -m 256M -cpu Icelake-Server-v1 -nographic -serial mon:stdio -s -S \
 -device loader,file=$FSP_T_FILE,addr=$FSP_T_ADDR \
--device loader,file=$BIOS_FILE,addr=0xFFFFD000 \
--device loader,addr=$FSP_T_ADDR_STORAGE,data=$FSP_T_ADDR,data-len=4 \
--device loader,addr=$((FSP_T_ADDR_STORAGE + 4)),data=$FSP_T_ADDR_COMPLEMENT,data-len=4
+-device loader,file=$MICRO_UPD_FILE,addr=$MICRO_UPD_ADDR \
+-device loader,file=$BIOS_FILE,addr=$BIOS_ADDR
+
+#-device loader,addr=$FSP_T_ADDR_STORAGE,data=$FSP_T_ADDR,data-len=4
+#-device loader,addr=$((FSP_T_ADDR_STORAGE + 4)),data=$FSP_T_ADDR_COMPLEMENT,data-len=4
 
 echo PID: $!
