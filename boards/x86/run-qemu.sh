ff8ef8b51a31e1ed78b97cdde8b0cadde1dc2157
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2024-09-16 10:07:01 +0000

    changes: 	QEMU invocation script now uses parameters.

--- boards/x86/run-qemu.sh
+++ boards/x86/run-qemu.sh
@@ -25,56 +25,112 @@
 #		POWER UP CODE
 #0x1.0000.0000 END OF 4GB MEMORY SPACE
 
-# This value contains address where FSP-T is placed
-FSP_T_ADDR=0x00010000
-printf "FSP_T_ADDR: 0x%08X\n" $FSP_T_ADDR
-FSP_T_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~FSP_T_ADDR) ))
-printf "FSP_T_ADDR_COMPLEMENT: 0x%08X\n" $FSP_T_ADDR_COMPLEMENT
+QEMU_ARGS="-m 256M -cpu Icelake-Server-v1 -nographic -serial mon:stdio -s -S"
 
-# This value contains address where Microcode Update is placed
-MICRO_UPD_ADDR=0x00020000
-printf "MICRO_UPD_ADDR: 0x%08X\n" $MICRO_UPD_ADDR
-MICRO_UPD_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~MICRO_UPD_ADDR) ))
-printf "MICRO_UPD_ADDR_COMPLEMENT: 0x%08X\n" $MICRO_UPD_ADDR_COMPLEMENT
+run_qemu() {
 
-# This value contains path and name of being loaded FSP-T file
-FSP_T_FILE=$1
+    if [ ${FSP_T_FILE} ]; then
 
-# This value contains path and name of being loaded Microcode Update file
-MICRO_UPD_FILE=$2
+	if ! [ ${FSP_T_ADDR} ]; then
+	    echo "FSP_T_ADDR isn't declared"
+	    exit 1
+	fi
 
-# This value contains address of FSP-T address
-BIN_TABLE=$(((1 << 32) - 0x40))
-printf "BIN_TABLE: 0x%08X\n" $BIN_TABLE
+	# This value contains path and name of being loaded FSP-T file
+	#FSP_T_FILE=$1
+	# This value contains address where FSP-T is placed
+	#FSP_T_ADDR=0x00010000
+	printf "FSP_T_FILE: %s\n" $FSP_T_FILE
+	printf "FSP_T_ADDR: 0x%08X\n" $FSP_T_ADDR
+	FSP_T_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~FSP_T_ADDR) ))
+	printf "FSP_T_ADDR_COMPLEMENT: 0x%08X\n" $FSP_T_ADDR_COMPLEMENT
+	printf "\n"
+	QEMU_ARGS+=" -device loader,file=$FSP_T_FILE,addr=$FSP_T_ADDR "
 
-# This value contains path and name of being loaded BIOS file
-BIOS_FILE=x86.bin
+    fi
 
-BIOS_SIZE=`stat --format="%s" $BIOS_FILE`
-printf "BIOS_SIZE: 0x%08X\n" $BIOS_SIZE
+    if [ ${MICRO_UPD_FILE} ]; then
 
-BIOS_ADDR=$(( (1<<32) - BIOS_SIZE ))
-printf "BIOS_ADDR: 0x%08X\n" $BIOS_ADDR
+	if ! [ ${MICRO_UPD_ADDR} ]; then
+	    echo "MICRO_UPD_ADDR isn't declared"
+	    exit 1
+	fi
 
-to_little_ending() {
-    V=`printf "%08x" $1`
-    V_LE=0x`echo ${V:6:2}${V:4:2}${V:2:2}${V:0:2}`
-    echo $V_LE
+	# This value contains path and name of being loaded Microcode Update file
+	#MICRO_UPD_FILE=$2
+	# This value contains address where Microcode Update is placed
+	#MICRO_UPD_ADDR=0x00020000
+	printf "MICRO_UPD_FILE: %s\n" $MICRO_UPD_FILE
+	printf "MICRO_UPD_ADDR: 0x%08X\n" $MICRO_UPD_ADDR
+	MICRO_UPD_ADDR_COMPLEMENT=$(( ((1 << 32) - 1) & (~MICRO_UPD_ADDR) ))
+	printf "MICRO_UPD_ADDR_COMPLEMENT: 0x%08X\n" $MICRO_UPD_ADDR_COMPLEMENT
+	printf "\n"
+	QEMU_ARGS+=" -device loader,file=$MICRO_UPD_FILE,addr=$MICRO_UPD_ADDR "
+
+    fi
+
+    # This value contains path and name of being loaded BIOS file
+    #BIOS_FILE=x86.bin
+    printf "BIOS_FILE: %s\n" $BIOS_FILE
+    BIOS_SIZE=`stat --format="%s" $BIOS_FILE`
+    printf "BIOS_SIZE: 0x%08X\n" $BIOS_SIZE
+    BIOS_ADDR=$(( (1<<32) - BIOS_SIZE ))
+    printf "BIOS_ADDR: 0x%08X\n" $BIOS_ADDR
+    # This value contains address of FSP-T address
+    BIN_TABLE=$(((1 << 32) - 0x40))
+    printf "BIN_TABLE: 0x%08X\n" $BIN_TABLE
+    QEMU_ARGS+=" -device loader,file=$BIOS_FILE,addr=$BIOS_ADDR "
+    printf "\n"
+
+    #echo "QEMU_ARGS: $QEMU_ARGS"
+
+    exec qemu-system-i386 $QEMU_ARGS
+
+    echo PID: $!
 }
 
-ADDR=$((0x3000 - 0x40 + 0x0)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $FSP_T_ADDR)`; echo $VAR | xxd -r - $BIOS_FILE
-ADDR=$((0x3000 - 0x40 + 0x4)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $FSP_T_ADDR_COMPLEMENT)`; echo $VAR | xxd -r - $BIOS_FILE
 
-ADDR=$((0x3000 - 0x40 + 0x8)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $MICRO_UPD_ADDR)`; echo $VAR | xxd -r - $BIOS_FILE
-ADDR=$((0x3000 - 0x40 + 0xC)); VAR=`printf "\"%08x: %08x\"" $ADDR $(to_little_ending $MICRO_UPD_ADDR_COMPLEMENT)`; echo $VAR | xxd -r - $BIOS_FILE
+if [ ${#} == 0 ]; then
+    echo "Invalid arguments"
+    exit -1
+fi
 
-qemu-system-i386 \
--m 256M -cpu Icelake-Server-v1 -nographic -serial mon:stdio -s -S \
--device loader,file=$FSP_T_FILE,addr=$FSP_T_ADDR \
--device loader,file=$MICRO_UPD_FILE,addr=$MICRO_UPD_ADDR \
--device loader,file=$BIOS_FILE,addr=$BIOS_ADDR
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+	"--bios")
+	    BIOS_FILE=${2}
+	    shift 
+	;;
 
-#-device loader,addr=$FSP_T_ADDR_STORAGE,data=$FSP_T_ADDR,data-len=4
-#-device loader,addr=$((FSP_T_ADDR_STORAGE + 4)),data=$FSP_T_ADDR_COMPLEMENT,data-len=4
+	"--mcupd")
+	    MICRO_UPD_FILE=${2}
+	    shift 
+	;;
 
-echo PID: $!
+	"--mcupd-addr")
+	    MICRO_UPD_ADDR=${2}
+	    shift 
+	;;
+
+	"--fspt")
+	    FSP_T_FILE=${2}
+	    shift 
+	;;
+
+	"--fspt-addr")
+	    FSP_T_ADDR=${2}
+	    shift 
+	;;
+
+	*)
+	    echo "Unknown parameter passed: $1"
+	    exit -1
+	;;
+    esac
+
+    shift
+done
+
+run_qemu
+
+#../boards/x86/run-qemu.sh --bios ./x86.bin --fspt ../Fsp_Rebased_T.fd --fspt-addr 0x00010000 --mcupd ../../microcodes/eaglestream_05.09.2023.mcb --mcupd-addr 0x00020000
